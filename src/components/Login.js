eb782c1d14c28613820ed65467c51876a6af1e7a
learntopilk <jonsterivilja@gmail.com>
2018-06-21 19:58:02 +0300

    7.4 tehty

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = ({ state, handleLoginFieldChange, login, visible, history }) => {
     return (
         <div>
                 <h4>Please log in</h4>
-                <form onSubmit={onSubmit}>
+                <form onSubmit={login}>
                     <div><span>username: <input type="text" name="username" onChange={handleLoginFieldChange} value={state.username} /></span></div>
                     <div><span>password: <input type="password" name="password" onChange={handleLoginFieldChange} value={state.password} /></span></div>
                     <button type="submit">login</button>                </form>
