428a2362f6cfd18e135a0b3080beb7cd9eb82d97
learntopilk <jonsterivilja@gmail.com>
2018-06-25 21:12:55 +0300

    Bäkkäri ja frontend nyt osana samaa repoa

---
+++ Blogs-Backend/controllers/blogs.js
@@ -0,0 +1,158 @@
+const jwt = require('jsonwebtoken')
+const blogsRouter = require('express').Router()
+const User = require('../models/user')
+const Blog = require('../models/blog')
+
+const formatBlogPost = (blog) => {
+  return {
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+    user: blog.user,
+    id: blog._id
+  }
+}
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({})
+    .populate('user', { name: 1, username: 1, _id: 1 })
+
+  //console.log(blogs)
+  response.json(blogs.map(b => formatBlogPost(b)))
+
+})
+
+blogsRouter.post('/', async (req, res) => {
+
+
+  try {
+    const token = req.body.token
+
+    if (!token) {
+      console.log('no token')
+      return res.status(400).send({ error: 'No token found in request' })
+    }
+    const decoded = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decoded.id) {
+      return res.status(401).send('Invalid or missing token')
+    }
+
+    if (!req.body.title || !req.body.url) {
+      console.log('title: ', req.body.title, 'url: ', req.body.url)
+      return res.status(400).send('Missing a title or url')
+    }
+
+    const user = await User.findById(decoded.id)
+
+    if (!user) {
+      return res.status(404).send('User not found!')
+    }
+
+    let author
+    if (!req.body.author || req.body.author === '') {
+      author = 'Unknown'
+    } else {
+      author = req.body.author
+    }
+
+    let likes
+    if (!req.body.likes) {
+      likes = 0
+    } else {
+      likes = req.body.likes
+    }
+
+    const blog = new Blog({
+      title: req.body.title,
+      author,
+      url: req.body.url,
+      likes,
+      user: user._id
+    })
+
+    const result = await blog.save()
+    console.log('blogs, notes, notes:')
+    console.log(user.blogs)
+    if (!user.blogs) {
+      user.blogs = []
+    }
+    console.log(user.blogs)
+
+    user.blogs = user.blogs.concat(result._id)
+    console.log(user.blogs)
+
+    await user.save()
+
+    res.json(formatBlogPost(result))
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ error: 'Something went seriously wrong.' })
+  }
+
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  let token
+  try {
+    token = request.body.token
+    if (!token) {
+      return response.status(403).json({ error: 'No token supplied with request' })
+    }
+  } catch (e) {
+    console.log(e)
+    return response.status(500)
+  }
+
+  const decoded = jwt.verify(token, process.env.SECRET)
+  if (!decoded.id) {
+    return response.status(401).send('Invalid token')
+  }
+
+  const blogToDelete = await Blog.findById(request.params.id)
+
+  if (!blogToDelete || !blogToDelete._id) {
+    return response.status(404).json({ error: 'Not found' })
+  }
+  console.log('check-up: ')
+  console.log(blogToDelete.user)
+  console.log(decoded.id)
+  if (blogToDelete.user.toString() !== decoded.id.toString() || !blogToDelete.user.toString() || blogToDelete.user.toString() === '') {
+    return response.status(403).send({ error: 'Incorrect user' })
+  }
+
+
+  console.log('deleting...')
+
+  await Blog.remove({ _id: request.params.id })
+    .catch(err => {
+      console.log('err: ', err)
+      return response.status(400)
+    })
+  response.status(200).end()
+  console.log('Deleted')
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  console.log('updating...')
+
+  const b = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: Number(request.body.likes)
+  }
+
+  const updatedB = await Blog
+    .findByIdAndUpdate({ _id: request.params.id }, b, { new: true })
+    .catch(err => {
+      console.log('error updating: ', err)
+      return response.status(404)
+    })
+  response.status(200).json(formatBlogPost(updatedB))
+
+})
+
+module.exports = blogsRouter
---
+++ Blogs-Frontend/src/App.js
@@ -0,0 +1,189 @@
+import React from 'react'
+import Blog from './components/Blog'
+import Login from './components/Login'
+import Togglable from './components/Togglable'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import blogService from './services/blogs'
+import BlogForm from './components/BlogForm'
+import { connect } from 'react-redux'
+import Notification from './components/Notification'
+import { notify } from './reducers/notificationReducer'
+import UserList from './components/UserList'
+import { initializeUsers } from './reducers/userReducer'
+import { initializeBlogs, createNewBlog } from './reducers/blogReducer'
+import { resetLoginInfo } from './reducers/loginInfoReducer'
+import User from './components/User'
+import { continueSession, initializeUser, logout } from './reducers/loggedInUserReducer';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blogtitle: '',
+      blogauthor: '',
+      blogurl: '',
+      loginVisible: false
+    }
+
+    this.deleteHandler = (id) => {
+      /*
+      let blogsToUpdate = []
+      for (let i = 0; i < this.state.blogs.length; i++) {
+        if (this.state.blogs[i].id === id) {
+        } else {
+          blogsToUpdate.push(this.state.blogs[i])
+        }
+      }
+      this.setState({ blogs: blogsToUpdate })
+      */
+    }
+
+
+
+  }
+
+  toggleLoginVisibility = () => {
+    this.setState({ loginVisible: !this.state.loginVisible })
+  }
+
+  logout = () => {
+    blogService.setToken(null)
+    console.log("logging out...")
+    this.props.logout()
+    console.log("Store after logout: ", this.props)
+    //this.props.history.push("/")
+  }
+
+  onBlogInputChange = (event) => {
+    event.preventDefault()
+    this.setState({ [event.target.name]: event.target.value })
+    console.log(`${[event.target.name]}: ${event.target.value}`)
+  }
+
+
+  onBlogSubmit = async (event) => {
+    event.preventDefault()
+
+    const blog = {
+      title: this.state.blogtitle,
+      author: this.state.blogauthor,
+      url: this.state.blogurl
+    }
+
+    try {
+      const result = await blogService.createBlogPost(blog)
+      console.log("Result of blog post creation: ", result)
+
+      let moddedRes = result
+      moddedRes.user = this.state.user
+
+      await this.props.createNewBlog(result)
+      this.setState({
+        blogtitle: '',
+        blogurl: '',
+        blogauthor: ''
+      }, () => { this.props.notify(`Added blog with title ${result.title} from author ${result.author}`, 5) })
+
+    } catch (err) {
+      console.log(err)
+      this.props.notify(`Bar request; something is wrong with your blog, dude`, 5)
+    }
+  }
+
+  userById = (id) => {
+    return this.props.users.find(u => u.id === id)
+  }
+
+  blogById = (id) => {
+    return this.props.blogs.find(b => b.id === id)
+  }
+
+  componentDidMount = async () => {
+    await this.props.initializeUsers()
+    await this.props.initializeBlogs()
+    await this.props.initializeUser()
+    await this.props.resetLoginInfo()
+    if (this.props.user) {
+      await blogService.setToken(this.props.user.token)
+    }
+
+    console.log("after init: ", this.props)
+
+    const userJSON = window.localStorage.getItem('loggedUser')
+    console.log("after componentDidMount: ", this.props)
+  }
+
+  render() {
+
+    console.log('user: ', this.props.user)
+
+    return (
+      <Router>
+        <div>
+          <h2>blogs</h2>
+          <Notification />
+          <div>
+            <Route exact path="/" render={({ history }) => {
+              if (this.props.user && this.props.user.token) {
+                return (
+                  <div>
+                    <div>
+                      <Link to="/">Home</Link> &nbsp;
+                      <Link to="/users">Users</Link> &nbsp;
+                      <span><em>{this.props.user.username} logged in</em></span> &nbsp;
+                      <button onClick={this.logout}>logout</button> &nbsp;
+                    </div>
+                    <Togglable buttonText={`Create new Blog Post (Fake news)`}>
+                      <BlogForm state={this.state} blogInputChangeHandler={this.onBlogInputChange} onBlogSubmit={this.onBlogSubmit} />
+                    </Togglable>
+                    <h3>Previous blogs: </h3>
+                    {this.props.blogs.sort((a, b) => { return b.likes - a.likes }).map(blog =>
+                      <h3 key={blog.id} className="blogLink"><a href={`/blogs/${blog.id}`}>{blog.title}</a></h3>
+                    )}
+                  </div>)
+              } else {
+                return (
+                  <div>
+                    <Link to="/">Home</Link> &nbsp;
+                    <Link to="/users">Users</Link> &nbsp;
+                    <span><em>User not logged in</em></span> &nbsp;
+
+                    <Togglable buttonText={`LOGin`}>
+                      <Login history={history} />
+                    </Togglable>
+                  </div>
+                )
+              }
+            }} />
+            <Route exact path="/users" render={() => <UserList />} />
+            <Route exact path="/users/:id" render={({ match }) => <User user={this.userById(match.params.id)} />} />
+            <Route exact path="/blogs/:id" render={({ match }) => <Blog blog={this.blogById(match.params.id)} />} />
+          </div>
+        </div>
+      </Router>
+
+    )
+  }
+}
+
+
+const mapStateToProps = (store) => {
+  return {
+    users: store.users,
+    blogs: store.blogs,
+    user: store.user
+  }
+}
+
+const mapDispatchToProps = {
+  notify,
+  initializeUsers,
+  initializeBlogs,
+  resetLoginInfo,
+  continueSession,
+  initializeUser,
+  logout,
+  createNewBlog
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
