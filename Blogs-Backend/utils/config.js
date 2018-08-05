50610c58346794a4226317b0ef54658d727ea97b
learntopilk <jonsterivilja@gmail.com>
2018-08-05 19:54:03 +0300

    Tehtävät lopetettu

--- Blogs-Backend/utils/config.js
+++ Blogs-Backend/utils/config.js
@@ -9,10 +9,12 @@ let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
+  console.log('TEST ENVIRONMENT')
   port = process.env.TEST_PORT
   mongoUrl = process.env.TEST_MONGODB_URI
+  console.log('Process.env: ', process.env)
 }
 
-console.log(port, mongoUrl, "PORT AND MONGOURL")
+console.log(port, mongoUrl, 'PORT AND MONGOURL')
 
 module.exports = { port, mongoUrl }
